27e3c390fb666ba84fd4a2ae986563bdbb5f476f
CodingLyf <codeandcarbs@gmail.com>
2025-03-16 13:39:40 +0530

    Rotate array by K steps

--- RotateArrayKSteps.java
+++ RotateArrayKSteps.java
@@ -5,17 +5,17 @@ import java.util.Arrays;
 public class RotateKStepsReverse {
 
 	public static void main(String[] args) {
-		int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
-		int k = 3;
+		int[] arr = { 1, 2, 3, 4, 5 };
+		int k = 2;
 		int n = arr.length;
 
-		reverse(arr, 0, n - 1);
+		reverse(arr, 0, n - 1); // Reverse entire array:
 		System.out.println("Reverse entire array: " + Arrays.toString(arr));
-		
-		reverse(arr, 0, k - 1); //
+
+		reverse(arr, 0, k - 1); // Reverse first k elements:
 		System.out.println("Reverse first k elements: " + Arrays.toString(arr));
-		
-		reverse(arr, k, n - 1);
+
+		reverse(arr, k, n - 1); // Step 3: Reverse last 3 (n-k) elements
 		System.out.println("Final output: " + Arrays.toString(arr));
 	}
 
